1451cbe2a910e49f182c29949c99f6262bc562c0
Gethvi <61206920+gethvi@users.noreply.github.com>
2020-05-30 03:05:09 +0200

    Updates version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="moodle-questions",
-    version="0.1.0",
+    version="0.2.1",
     author="Gethvi",
     description="Library for manipulating questions in Moodle XML format.",
     url="https://github.com/gethvi/moodle-questions",
